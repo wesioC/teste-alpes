f867061d9d4e23e17317e713b4018d6f1c0fb237
wesioC <wesiosp@gmail.com>
2025-08-26 14:30:49 -0300

    feat: criado o comando para importar os dados
    fix: ajuste na model para aceitar a injeção de id sem auto-increment

--- app/Models/Carro.php
+++ app/Models/Carro.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Carro extends Model
 {
-    //
+    protected $table = 'carros';
+
+    protected $fillable = [
+        'id',
+        'type',
+        'brand',
+        'model',
+        'version',
+        'year_model',
+        'year_build',
+        'optionals',
+        'doors',
+        'board',
+        'chassi',
+        'transmission',
+        'km',
+        'description',
+        'sold',
+        'category',
+        'url_car',
+        'old_price',
+        'price',
+        'color',
+        'fuel',
+        'fotos',
+        'created_at',
+        'updated_at',
+    ];
 }
--- database/migrations/2025_08_26_144622_create_carros_table.php
+++ database/migrations/2025_08_26_144622_create_carros_table.php
@@ -13,6 +13,27 @@ return new class extends Migration
     {
         Schema::create('carros', function (Blueprint $table) {
             $table->id();
+            $table->string('type')->nullable();
+            $table->string('brand')->nullable();
+            $table->string('model')->nullable();
+            $table->string('version')->nullable();
+            $table->year('year_model')->nullable();
+            $table->year('year_build')->nullable();
+            $table->json('optionals')->nullable();
+            $table->integer('doors')->nullable();
+            $table->string('board')->nullable();
+            $table->string('chassi')->nullable();
+            $table->string('transmission')->nullable();
+            $table->integer('km')->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('sold')->default(false);
+            $table->string('category')->nullable();
+            $table->string('url_car')->nullable();
+            $table->decimal('old_price', 10, 2)->nullable();
+            $table->decimal('price', 10, 2)->nullable();
+            $table->string('color')->nullable();
+            $table->string('fuel')->nullable();
+            $table->json('fotos')->nullable();
             $table->timestamps();
         });
     }
